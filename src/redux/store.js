590f85d229ffb5ba16916f0a23d618bcf8ded687
monika <monikacsns14@gmail.com>
2026-04-07 16:57:24 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,24 +8,24 @@ import { enrollmentApi } from "./api/enrollmentApi";
 import { callNotesApi } from "./api/callNotesApi";
 
 export const store = configureStore({
-  reducer: {
-    [authApi.reducerPath]: authApi.reducer,
-    [profileApi.reducerPath]: profileApi.reducer,
-    [offersApi.reducerPath]: offersApi.reducer,
-    [servicesApi.reducerPath]: servicesApi.reducer,
-    [advertisementsApi.reducerPath]: advertisementsApi.reducer,
-    [enrollmentApi.reducerPath]: enrollmentApi.reducer,
-    [callNotesApi.reducerPath]: callNotesApi.reducer,
-  },
+    reducer: {
+        [authApi.reducerPath]: authApi.reducer,
+        [profileApi.reducerPath]: profileApi.reducer,
+        [offersApi.reducerPath]: offersApi.reducer,
+        [servicesApi.reducerPath]: servicesApi.reducer,
+        [advertisementsApi.reducerPath]: advertisementsApi.reducer,
+        [enrollmentApi.reducerPath]: enrollmentApi.reducer,
+        [callNotesApi.reducerPath]: callNotesApi.reducer,
+    },
 
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
-      authApi.middleware,
-      profileApi.middleware,
-      offersApi.middleware,
-      servicesApi.middleware,
-      advertisementsApi.middleware,
-      enrollmentApi.middleware,
-      callNotesApi.middleware
-    ),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(
+            authApi.middleware,
+            profileApi.middleware,
+            offersApi.middleware,
+            servicesApi.middleware,
+            advertisementsApi.middleware,
+            enrollmentApi.middleware,
+            callNotesApi.middleware
+        ),
 });
